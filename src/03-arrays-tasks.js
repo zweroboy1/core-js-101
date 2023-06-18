c5fc28edcb3ce753a5657898a119568b6a7fe67f
Konrad <fprognoz@gmail.com>
2023-06-18 22:16:26 +0300

    feat: add getPositivesCount

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -307,8 +307,8 @@ function get3TopItems(arr) {
  *   [ null, 1, 'elephant' ] => 1
  *   [ 1, '2' ] => 1
  */
-function getPositivesCount(/* arr */) {
-  throw new Error('Not implemented');
+function getPositivesCount(arr) {
+  return arr.reduce((acc, number) => acc + Number(typeof number === 'number' && number > 0), 0);
 }
 
 /**

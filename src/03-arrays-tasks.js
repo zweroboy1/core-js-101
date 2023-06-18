b1fd9c5a88f46c195594768213d1d6c4ff4c630e
Konrad <fprognoz@gmail.com>
2023-06-18 20:50:02 +0300

    feat: add propagateItemsByPositionIndex

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -271,10 +271,11 @@ function getSecondItems(arr) {
  *  [ 'a', 'b', 'c', null ] => [ 'a', 'b','b', 'c','c','c',  null,null,null,null ]
  *  [ 1,2,3,4,5 ] => [ 1, 2,2, 3,3,3, 4,4,4,4, 5,5,5,5,5 ]
  */
-function propagateItemsByPositionIndex(/* arr */) {
-  throw new Error('Not implemented');
+function propagateItemsByPositionIndex(arr) {
+  const result = [];
+  arr.map((el, index) => result.push(...Array(index + 1).fill(el)));
+  return result;
 }
-
 
 /**
  * Returns the 3 largest numbers from the specified array

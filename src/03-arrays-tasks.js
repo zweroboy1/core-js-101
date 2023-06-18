c9c371837269f67ec95fb926b2d930e18dabc60d
Konrad <fprognoz@gmail.com>
2023-06-18 22:32:23 +0300

    feat: add getFalsyValuesCount

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -359,8 +359,8 @@ function getItemsSum(arr) {
  *  [ -1, 'false', null, 0 ] => 2
  *  [ null, undefined, NaN, false, 0, '' ]  => 6
  */
-function getFalsyValuesCount(/* arr */) {
-  throw new Error('Not implemented');
+function getFalsyValuesCount(arr) {
+  return arr.reduce((acc, el) => acc + Number(!el), 0);
 }
 
 /**

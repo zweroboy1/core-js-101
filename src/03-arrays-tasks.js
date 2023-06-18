6c8f5454fa2d55a51d35b648443ac2d4acdf48d9
Konrad <fprognoz@gmail.com>
2023-06-18 20:26:44 +0300

    feat: add getMovingSum

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -232,8 +232,13 @@ function toArrayOfSquares(arr) {
  *   [ 0, 0, 0, 0, 0]         => [ 0, 0, 0, 0, 0]
  *   [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ] => [ 1, 3, 6, 10, 15, 21, 28, 36, 45, 55 ]
  */
-function getMovingSum(/* arr */) {
-  throw new Error('Not implemented');
+function getMovingSum(arr) {
+  let sum = 0;
+  return arr.map((el) => {
+    const current = el + sum;
+    sum = current;
+    return current;
+  });
 }
 
 /**

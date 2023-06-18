90618cb4ab99b150802d7011bf5116e9e20561a4
Konrad <fprognoz@gmail.com>
2023-06-18 14:45:26 +0300

    feat: add concatenateStrings

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -18,8 +18,8 @@
  *   'aa',''    => 'aa'
  *   '',  'bb'  => 'bb'
  */
-function concatenateStrings(/* value1, value2 */) {
-  throw new Error('Not implemented');
+function concatenateStrings(value1 = '', value2 = '') {
+  return String(value1) + String(value2);
 }
 
 

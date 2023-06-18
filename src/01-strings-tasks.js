eece229de228cbc3c60a077b57f3392433cc7708
Konrad <fprognoz@gmail.com>
2023-06-18 16:07:25 +0300

    feat: add isString

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -251,8 +251,8 @@ function encodeToRot13(str) {
  *   isString('test') => true
  *   isString(new String('test')) => true
  */
-function isString(/* value */) {
-  throw new Error('Not implemented');
+function isString(value) {
+  return typeof value === 'string' || value instanceof String;
 }
 
 

a11114331dd707d207316957f26caa8f2be0e9b1
Konrad <fprognoz@gmail.com>
2023-06-18 20:30:19 +0300

    feat: add getSecondItems

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -252,8 +252,8 @@ function getMovingSum(arr) {
  * [ 'a', 'b', 'c' , null ]  => [ "b", null ]
  * [ "a" ] => []
  */
-function getSecondItems(/* arr */) {
-  throw new Error('Not implemented');
+function getSecondItems(arr) {
+  return arr.filter((el, index) => index % 2);
 }
 
 

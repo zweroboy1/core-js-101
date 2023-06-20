79b0fd5556c7e37cbf4ee3d13fc61102ff66f60f
Konrad <fprognoz@gmail.com>
2023-06-20 19:39:20 +0300

    feat: add toStringList

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -392,8 +392,8 @@ function findAllOccurrences(arr, item) {
  *    [1, 2, 3, 4, 5]                   => '1,2,3,4,5'
  *    ['rock', 'paper', 'scissors']     => 'rock,paper,scissors'
  */
-function toStringList(/* arr */) {
-  throw new Error('Not implemented');
+function toStringList(arr) {
+  return arr.join(',');
 }
 
 

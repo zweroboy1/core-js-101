eca7a064f0c92755a900d08dcc2bbb42552338e4
Konrad <fprognoz@gmail.com>
2023-06-20 19:38:04 +0300

    feat: add findAllOccurrences

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -377,8 +377,8 @@ function getFalsyValuesCount(arr) {
  *    [ null, undefined, null ], null => 2
  *    [ true, 0, 1, 'true' ], true => 1
  */
-function findAllOccurrences(/* arr, item */) {
-  throw new Error('Not implemented');
+function findAllOccurrences(arr, item) {
+  return arr.filter((el) => el === item).length;
 }
 
 /**

b13be7bb5d1a611befdb2cc515ade1c6431f7872
Konrad <fprognoz@gmail.com>
2023-06-20 21:19:22 +0300

    feat: add selectMany

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -542,10 +542,9 @@ function group(array, keySelector, valueSelector) {
  *   [[1, 2], [3, 4], [5, 6]], (x) => x     =>   [ 1, 2, 3, 4, 5, 6 ]
  *   ['one','two','three'], (x) => x.split('')  =>   ['o','n','e','t','w','o','t','h','r','e','e']
  */
-function selectMany(/* arr, childrenSelector */) {
-  throw new Error('Not implemented');
+function selectMany(arr, childrenSelector) {
+  return arr.flatMap((el) => childrenSelector(el));
 }
-
 
 /**
  * Returns an element from the multidimensional array by the specified indexes.

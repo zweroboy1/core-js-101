808c5e2beca92cd7ffccdf8c5ebce03c273ef07d
Konrad <fprognoz@gmail.com>
2023-06-20 20:53:11 +0300

    feat: add distinct

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -487,8 +487,8 @@ function getIntervalArray(start, end) {
  *   [ 'a', 'a', 'a', 'a' ]  => [ 'a' ]
  *   [ 1, 1, 2, 2, 3, 3, 4, 4] => [ 1, 2, 3, 4]
  */
-function distinct(/* arr */) {
-  throw new Error('Not implemented');
+function distinct(arr) {
+  return Array.from(new Set(arr));
 }
 
 /**

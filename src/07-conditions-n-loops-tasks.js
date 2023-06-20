f9aec2f30bb108a76b32294b502d3a40cb888d0f
Konrad <fprognoz@gmail.com>
2023-06-20 22:01:28 +0300

    feat: add getSumBetweenNumbers

--- src/07-conditions-n-loops-tasks.js
+++ src/07-conditions-n-loops-tasks.js
@@ -73,8 +73,8 @@ function getFactorial(n) {
  *   5,10  =>  45 ( = 5+6+7+8+9+10 )
  *   -1,1  =>  0  ( = -1 + 0 + 1 )
  */
-function getSumBetweenNumbers(/* n1, n2 */) {
-  throw new Error('Not implemented');
+function getSumBetweenNumbers(n1, n2) {
+  return (n1 + n2) * (n2 - n1 + 1) * 0.5;
 }
 
 

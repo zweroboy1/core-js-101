2945243cf0dbed979d5ae5f8d3add8e9d50a9d95
Konrad <fprognoz@gmail.com>
2023-06-20 19:49:18 +0300

    feat: add sortCitiesArray

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -423,8 +423,16 @@ function toStringList(arr) {
  *      { country: 'Russia',  city: 'Saint Petersburg' }
  *    ]
  */
-function sortCitiesArray(/* arr */) {
-  throw new Error('Not implemented');
+function sortCitiesArray(arr) {
+  return arr.sort((a, b) => {
+    if (a.country > b.country) {
+      return 1;
+    }
+    if (a.country < b.country) {
+      return -1;
+    }
+    return a.city > b.city ? 1 : -1;
+  });
 }
 
 /**

4d23cbaea404f88511118ab17043110f19b3c1ea
Konrad <fprognoz@gmail.com>
2023-06-21 17:19:36 +0300

    feat: add Rectangle

--- src/05-objects-tasks.js
+++ src/05-objects-tasks.js
@@ -20,8 +20,11 @@
  *    console.log(r.height);      // => 20
  *    console.log(r.getArea());   // => 200
  */
-function Rectangle(/* width, height */) {
-  throw new Error('Not implemented');
+function Rectangle(width, height) {
+  this.width = width;
+  this.height = height;
+  this.getArea = () => this.width * this.height;
+  return this;
 }
 
 

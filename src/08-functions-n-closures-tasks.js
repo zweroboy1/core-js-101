8ab77b147dc7cc5d39585833ae1fa6d35934aca5
Konrad <fprognoz@gmail.com>
2023-06-21 14:03:20 +0300

    feat: add getPowerFunction

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -43,8 +43,8 @@ function getComposition(f, g) {
  *   power05(16) => 4
  *
  */
-function getPowerFunction(/* exponent */) {
-  throw new Error('Not implemented');
+function getPowerFunction(exponent) {
+  return (number) => number ** exponent;
 }
 
 

258d4443bfabf94505f1a47b8be587f593c99e22
Konrad <fprognoz@gmail.com>
2023-06-21 17:23:49 +0300

    feat: add fromJSON

--- src/05-objects-tasks.js
+++ src/05-objects-tasks.js
@@ -54,8 +54,10 @@ function getJSON(obj) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON(/* proto, json */) {
-  throw new Error('Not implemented');
+function fromJSON(proto, json) {
+  const obj = JSON.parse(json);
+  Object.setPrototypeOf(obj, proto);
+  return obj;
 }
 
 
